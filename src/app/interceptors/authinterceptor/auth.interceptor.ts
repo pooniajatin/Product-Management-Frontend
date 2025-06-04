5faf7598d1ce873a22f1372adb6bb90faa9da36c
Jatin Poonia <jatin.poonia@unthinkable.com>
2025-06-04 12:57:16 +0530

    removed console

--- src/app/interceptors/authinterceptor/auth.interceptor.ts
+++ src/app/interceptors/authinterceptor/auth.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('Bearer');
-  console.log("reqqqqqqqqqq",token)
+  
   let cloned=req
   if(token){
     cloned = req.clone({
