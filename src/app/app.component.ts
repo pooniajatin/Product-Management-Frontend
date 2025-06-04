967b65dc666e9bd9efbebfbd79fdb5a151de070c
Jatin Poonia <jatin.poonia@unthinkable.com>
2025-06-04 09:18:33 +0530

    Navigation bar implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet ,Router} from '@angular/router';
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 
 @Component({
+  standalone:true,
   selector: 'app-root',
-  imports: [ RouterOutlet,],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Login-Frontend';
+  constructor(private router:Router){}
+   navigateTo(path:string){
+    this.router.navigate([path])
+   }
 }
