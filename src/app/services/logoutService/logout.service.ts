adfba482e077a30ae2a72202e4015b9c4f0497f3
Jatin Poonia <jatin.poonia@unthinkable.com>
2025-06-04 09:52:22 +0530

    implemented logout service

---
+++ src/app/services/logoutService/logout.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class LogoutService {
+ 
+  constructor(private http:HttpClient) { }
+  private baseUrl = 'http://localhost:3000/api/v1'
+   logoutUser(data:any):Observable<any>{
+    return this.http.post(`${this.baseUrl}/logout`,data);
+   }
+}
