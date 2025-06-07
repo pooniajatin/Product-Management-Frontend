294e3f738d84fb71aa6d5d52f010888178887468
Jatin Poonia <jatin.poonia@unthinkable.com>
2025-06-07 13:20:10 +0530

    created profile services

---
+++ src/app/services/profileService/profile.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http : HttpClient) { }
+   private baseUrl = 'http://localhost:3000/api/v1'
+   getProfile(id:any):Observable<any>{
+    return this.http.get(`${this.baseUrl}/profile/${id}`,{withCredentials: true})
+   }
+   createProfile(data:any):Observable<any>{
+    return this.http.post(`${this.baseUrl}/profile`,data,{withCredentials: true})
+   }
+   deleteProfile(id:any):Observable<any>{
+    return this.http.delete(`${this.baseUrl}/profile/${id}`)
+   }
+   updateProfile(id:any,data:any):Observable<any>{
+    return this.http.patch(`${this.baseUrl}/profile/${id}`,data,{withCredentials: true})
+   }
+}
